a5363922fb7bcef0147403f122f11aecc4f941ab
nhadi3 <nhadi3@illinois.edu>
2019-02-05 23:34:17 -0600

    Added tests for helper method and created working runAdventure.

--- src/com/example/Adventure.java
+++ src/com/example/Adventure.java
@@ -8,27 +8,95 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Adventure {
     private static final int STATUS_OK = 200;
     private static final String URL = requestURLFromUser();
-
-    public static void main(String [] arguments) throws Exception {
-    }
+    private static boolean GAME_ENDED = false;
+    private static Layout LAYOUT = new Layout();
 
     public static Layout makeApiRequest(String url) throws UnirestException, MalformedURLException {
         final HttpResponse<String> stringHttpResponse;
         new URL(url);
-        Layout layout = new Layout();
         stringHttpResponse = Unirest.get(url).asString();
 
         if (stringHttpResponse.getStatus() == STATUS_OK) {
             String json = stringHttpResponse.getBody();
             Gson gson = new Gson();
-            layout = gson.fromJson(json, Layout.class);
+            LAYOUT = gson.fromJson(json, Layout.class);
         }
-        return layout;
+        return LAYOUT;
+    }
+
+    public static Room getStartingRoomDirections() {
+        Room[] rooms = LAYOUT.getRooms();
+        ArrayList<Room> roomsArray = new ArrayList<>(Arrays.asList(rooms));
+        Room currentRoom = new Room();
+        String startingRoom = LAYOUT.getStartingRoom();
+        for (int i = 0; i < roomsArray.size(); i++) {
+            if (roomsArray.get(i).getName().equals(startingRoom)) {
+                currentRoom = roomsArray.get(i);
+            }
+        }
+        return currentRoom;
+    }
+
+    private static void runAdventure() throws Exception {
+        Layout layout = makeApiRequest(URL);
+        Room[] rooms = layout.getRooms();
+        ArrayList<Room> roomsArray = new ArrayList<>(Arrays.asList(rooms));
+        String newRoom = "";
+        Room currentRoom = getStartingRoomDirections();
+        while (!GAME_ENDED) {
+            //Print out description of room and possible directions.
+            System.out.println(currentRoom.getDescription());
+            System.out.println(printDirections(currentRoom));
+            Direction[] directions = currentRoom.getDirections();
+            ArrayList<Direction> directionsArray = new ArrayList<>(Arrays.asList(directions));
+
+            //Get user input.
+            Scanner input = new Scanner(System.in);
+            String command = input.nextLine().toLowerCase();
+
+            //Check if user quits game.
+            if (checkIfUserQuits(command).equals("User quit.")) {
+                System.exit(0);
+            }
+
+            //Check if command is valid and print out respective output.
+            if (command.length() < 3) {
+                System.out.println("I don't understand " + command);
+            } else if (command.substring(0, 3).equals("go ")) {
+                for (int i = 0; i < directionsArray.size(); i++) {
+                    if (command.substring(3).equalsIgnoreCase(directionsArray.get(i).getDirectionName())) {
+                        newRoom = directionsArray.get(i).getRoom();
+                    }
+                }
+                if (currentRoom.getName().equals(newRoom)) {
+                    System.out.println("I can't " + command);
+                }
+            } else {
+                System.out.println("I don't understand " + command);
+            }
+
+            //Set newRoom to the currentRoom object.
+            for (int i = 0; i < roomsArray.size(); i++) {
+                if (roomsArray.get(i).getName().equals(newRoom)) {
+                    currentRoom = roomsArray.get(i);
+                }
+            }
+
+            //Check if game has ended.
+            if (newRoom.equals(layout.getEndingRoom())) {
+                GAME_ENDED = true;
+                System.out.println(currentRoom.getDescription());
+                System.out.println("You have reached your final destination and exit.");
+                System.exit(0);
+            }
+        }
     }
 
     private static String requestURLFromUser() {
@@ -44,5 +112,28 @@ public class Adventure {
             System.out.println("That URL is not valid: " + url);
         }
         return url;
+    }
+
+    public static String printDirections(Room currentRoom) {
+        Direction[] directions = currentRoom.getDirections();
+        ArrayList<Direction> directionsArray = new ArrayList<>(Arrays.asList(directions));
+        String[] directionsNameArray = new String[directionsArray.size()];
+        for (int i = 0; i < directionsArray.size(); i++) {
+            directionsNameArray[i] = directionsArray.get(i).getDirectionName();
+        }
+        String array = Arrays.toString(directionsNameArray).replace("[", "")
+                .replace("]", "");
+        return "From here you can go: " + array + ".";
+    }
+
+    public static String checkIfUserQuits(String command) {
+        if (command.equals("quit") || command.equals("exit")) {
+            return "User quit.";
+        }
+        return "User has not quit.";
+    }
+
+    public static void main(String [] arguments) throws Exception{
+        runAdventure();
     }
 }
